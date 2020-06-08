f9d430244a8378d142c7fb216617541fe2b8c08d
Choudhury Iqbal <ChoudhuryIqbal@users.noreply.github.com>
2020-06-08 15:44:13 -0400

    Updated All classes for CustomerOrder Service App

---
+++ CustomerOrderService/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerOrderService
+{
+   public class Customer
+    {
+
+        public int CustomerId { get; set; }
+        public int CustomerName { get; set; }
+        public CustomerType CustomerType { get; set; }
+    }
+}
